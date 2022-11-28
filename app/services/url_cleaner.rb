ab0a04b96f5989a3dffb7e191a843d3473b6e990
sethherr <seth.william.herr@gmail.com>
2022-11-28 11:09:01 -0600

    add url cleaner

---
+++ app/services/url_cleaner.rb
@@ -0,0 +1,67 @@
+class UrlCleaner
+  class << self
+    def base_domains(str)
+      str = "http://#{str}" unless str.match?(/\Ahttp/i) # uri parse doesn't work without protocol
+      uri = URI.parse(encoded_url(str))
+      base_domain = uri.host&.downcase
+      # Unless the base_domain has "." and some characters, assume it's not a domain
+      return [] unless base_domain.present? && base_domain.match?(/\..+/)
+      return ["wikipedia.org"] if base_domain.match?(/wikipedia.org\z/) # Just return wikipedia
+      # If the domain starts with www. add both that and the bare domain
+      base_domain.start_with?(/www\./) ? [base_domain, base_domain.delete_prefix("www.")] : [base_domain]
+    rescue URI::InvalidURIError
+      []
+    end
+
+    def base_domain_without_www(str)
+      base_domains(str).last # Last in array will not have www
+    end
+
+    def pretty_url(str)
+      return str unless str.present?
+      without_utm(str)
+        .gsub(/\Ahttps?:\/\//i, "") # Remove https
+        .gsub(/\Awww\./i, "") # Remove www
+    end
+
+    def without_utm(str)
+      return nil unless str.present?
+      without_anchor(str)
+        .gsub(/&?utm_.+?(&|$)/i, "") # Remove UTM parameters
+        .gsub(/\/?\??\z/, "") # Remove trailing slash and ?
+    end
+
+    def without_anchor(str)
+      return nil unless str.present?
+      str.strip.gsub(/#.*\z/, "")
+    end
+
+    def with_http(str)
+      return str unless str.present? && str.match?(/\./)
+      result = str.start_with?(/http/i) ? str : "http://#{str}"
+      URI.parse(result).host && result
+    rescue URI::InvalidURIError
+      str
+    end
+
+    def without_base_domain(str)
+      return nil unless str.present?
+      # Get the first domain - which will be the actual base domain passed in
+      base_domain = base_domains(str).first
+      return str unless base_domain.present?
+      return base_domain unless str.split(base_domain).count > 1
+      str.split(base_domain).last&.gsub(/\A\//, "")&.gsub(/\/\z/, "")
+    end
+
+    # escape non-escaped sequences in the URI - e.g. spaces and mdashes
+    def encoded_url(str)
+      URI::DEFAULT_PARSER.escape(str)
+    end
+
+    def looks_like_url?(str)
+      return false if str.blank?
+      return false if str.strip.match?(/\s/)
+      str.match?(/\//) || str.match?(/\.\w+/)
+    end
+  end
+end
---
+++ spec/services/url_cleaner_spec.rb
@@ -0,0 +1,114 @@
+require "rails_helper"
+
+RSpec.describe UrlCleaner do
+  let(:subject) { described_class }
+
+  describe "base_domains" do
+    it "returns array of domain with www and without" do
+      expect(subject.base_domains("https://www.nationalreview.com/2020/09/the-cdcs-power-grab/")).to eq(["www.nationalreview.com", "nationalreview.com"])
+    end
+    context "non-www subdomain" do
+      it "returns just one domain" do
+        # Doing wikipedia domains here, because I ran into this problem with wikipedia, but we're handling wikipedia specially
+        expect(subject.base_domains("https://en.coolpedia.org/wiki/John_von_Neumann")).to eq(["en.coolpedia.org"])
+        expect(subject.base_domains("https://en.m.coolpedia.org/wiki/John_von_Neumann")).to eq(["en.m.coolpedia.org"])
+      end
+    end
+    context "wikipedia" do
+      it "returns wikipedia" do
+        expect(subject.base_domains("https://en.wikipedia.org/wiki/John_von_Neumann")).to eq(["wikipedia.org"])
+      end
+    end
+  end
+
+  describe "base_domain_without_www" do
+    it "gets the domain without www" do
+      expect(subject.base_domain_without_www("https://www.nationalreview.com/2020/09/the-cdcs-power-grab/")).to eq("nationalreview.com")
+    end
+    it "includes non-www subdomain" do
+      expect(subject.base_domain_without_www("https://en.coolpedia.org/wiki/John_von_Neumann")).to eq "en.coolpedia.org"
+    end
+    it "handles without http" do
+      expect(subject.base_domain_without_www("coolpedia.org/wiki/John_von_Neumann")).to eq "coolpedia.org"
+    end
+    context "wikipedia" do
+      it "returns wikipedia" do
+        expect(subject.base_domain_without_www("https://en.wikipedia.org/wiki/John_von_Neumann")).to eq "wikipedia.org"
+        expect(subject.base_domain_without_www("https://en.m.wikipedia.org/wiki/John_von_Neumann")).to eq "wikipedia.org"
+        expect(subject.base_domains("https://en.m.wikipedia.org")).to eq(["wikipedia.org"])
+      end
+      it "doesn't shit the bed on non-percent encoded URLs" do
+        expect(subject.base_domain_without_www("https://en.m.wikipedia.org/wiki/Glass–Steagall_legislation")).to eq "wikipedia.org"
+      end
+    end
+  end
+
+  describe "without_base_domain" do
+    it "returns the string if it doesn't seem like a url" do
+      expect(subject.without_base_domain("This isn't a URL")).to eq "This isn't a URL"
+    end
+    it "returns without the base domain" do
+      expect(subject.without_base_domain("https://www.nationalreview.com/2020/09/the-cdcs-power-grab/")).to eq "2020/09/the-cdcs-power-grab"
+    end
+    it "returns the domain if there is no query" do
+      expect(subject.without_base_domain("https://bikeindex.org")).to eq "bikeindex.org"
+      expect(subject.without_base_domain("http://example.com")).to eq "example.com"
+    end
+  end
+
+  describe "pretty_url" do
+    it "returns without the protocol and trailing stuff" do
+      expect(subject.pretty_url("https://en.wikipedia.org/wiki/John_von_Neumann/")).to eq "en.wikipedia.org/wiki/John_von_Neumann"
+      expect(subject.pretty_url("http://en.wikipedia.org/wiki/John_von_Neumann?")).to eq "en.wikipedia.org/wiki/John_von_Neumann"
+      expect(subject.pretty_url("http://en.wikipedia.org/wiki/John_von_Neumann/?")).to eq "en.wikipedia.org/wiki/John_von_Neumann"
+    end
+    it "returns without UTM parameters" do
+      target = "nationalreview.com/2020/09/bring-back-the-bison/?somethingimportant=33333utm"
+      expect(subject.pretty_url(" www.nationalreview.com/2020/09/bring-back-the-bison/?utm_source=recirc-desktop&utm_medium=article&UTM_CAMPAIGN=river&somethingimportant=33333utm&utm_content=top-bar-latest&utm_term=second")).to eq target
+    end
+  end
+
+  describe "without_utm" do
+    it "returns nil" do
+      expect(subject.without_utm("   \n")).to eq(nil)
+    end
+    it "returns without UTM parameters" do
+      target = "https://www.nationalreview.com/2020/09/bring-back-the-bison/?somethingimportant=33333utm"
+      expect(subject.without_utm("https://www.nationalreview.com/2020/09/bring-back-the-bison/?utm_source=recirc-desktop&utm_medium=article&UTM_CAMPAIGN=river&somethingimportant=33333utm&utm_content=top-bar-latest&utm_term=second")).to eq target
+    end
+    it "returns without anchor" do
+      target = "https://en.wikipedia.org/wiki/Rationale_for_the_Iraq_War?somethingimportant=true"
+      expect(subject.without_utm("https://en.wikipedia.org/wiki/Rationale_for_the_Iraq_War?somethingimportant=true#cite_note-10")).to eq target
+    end
+  end
+
+  describe "with_http" do
+    it "returns with http" do
+      expect(subject.with_http("example.com")).to eq "http://example.com"
+      expect(subject.with_http("http://example.com")).to eq "http://example.com"
+      expect(subject.with_http(subject.without_utm("example.com"))).to eq "http://example.com"
+    end
+    it "doesn't modify https" do
+      expect(subject.with_http("https://www.nationalreview.com/2020/09/?")).to eq "https://www.nationalreview.com/2020/09/?"
+    end
+    it "non-urls returns without http" do
+      expect(subject.with_http("whatever")).to eq "whatever"
+    end
+  end
+
+  describe "looks_like_url?" do
+    it "is true for url" do
+      expect(subject.looks_like_url?("https://www.nationalreview.com/2020/09/?")).to be_truthy
+    end
+    it "is true for url without protocol" do
+      expect(subject.looks_like_url?("www.nationalreview.com/2020/09/?")).to be_truthy
+      expect(subject.looks_like_url?("www.nationalreview.com")).to be_truthy
+    end
+    it "is false for sentence" do
+      expect(subject.looks_like_url?("quick brown fox")).to be_falsey
+    end
+    it "is false for blank" do
+      expect(subject.looks_like_url?(" ")).to be_falsey
+    end
+  end
+end
