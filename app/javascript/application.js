3cecd1787ca52f4f884cc04af4c8c506ae5dd8c8
Seth Herr <seth.william.herr@gmail.com>
2023-02-14 08:29:43 -0800

    Checkup, run it (#2)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,10 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// import './controllers'
+// Entry point for the build script in your package.json
+import '@hotwired/turbo-rails'
+import jQuery from 'jquery'
+import './controllers'
+
+document.addEventListener('turbo:load', () => {
+  console.log('party')
+})
+
+window.$ = window.jQuery = jQuery
