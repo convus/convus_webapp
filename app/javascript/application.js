b8dcb82934a726425df08d4c12f637f09ab3557c
sethherr <seth.william.herr@gmail.com>
2023-02-22 12:46:43 -0800

    wtf, pointer

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,9 @@
 // Entry point for the build script in your package.json
 import '@hotwired/turbo-rails'
-import jQuery from 'jquery'
 import './controllers'
+// Import flowbite, a tailwind component library, for interactions
+import 'flowbite/dist/flowbite.turbo.js'
 
 document.addEventListener('turbo:load', () => {
   console.log('party')
 })
-
-window.$ = window.jQuery = jQuery
--- postcss.config.js
+++ postcss.config.js
@@ -10,9 +10,31 @@ module.exports = {
         './public/*.html',
         './app/helpers/**/*.rb',
         './app/javascript/**/*.js',
-        './app/views/**/*.erb'
+        './app/views/**/*',
+        './node_modules/flowbite/**/*.js' // JS interactions
       ],
       theme: {
+        colors: {
+          transparent: 'transparent',
+          current: 'currentColor',
+          white: '#ffffff',
+          black: '#000000',
+          primary: '#0d6efd',
+          success: '#75b798', // alternative: #a3cfbb
+          error: '#fecba1', // orange
+          gray: {
+            DEFAULT: '#adb5bd', // bootstrap grays
+            100: '#f8f9fa',
+            200: '#e9ecef',
+            300: '#dee2e6',
+            400: '#ced4da',
+            500: '#adb5bd',
+            600: '#6c757d',
+            700: '#495057',
+            800: '#343a40',
+            900: '#212529'
+          }
+        },
         extend: {
           fontFamily: {
             serif: ['Iowan Old Style', 'Apple Garamond', 'Baskerville', 'Times New Roman', 'Droid Serif', 'Times', 'Source Serif Pro', 'serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol']
