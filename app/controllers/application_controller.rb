7201b77eb6a0bbde7c133f654fb7808260de7ff7
sethherr <seth.william.herr@gmail.com>
2023-03-05 18:54:40 -0800

    permit username assignment on signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -48,6 +48,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
