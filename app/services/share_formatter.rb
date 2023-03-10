e6cd9f275fa8f1c6bdfe2293fbcdf79f99e84b88
sethherr <seth.william.herr@gmail.com>
2023-03-10 09:39:29 -0800

    more things

--- app/services/share_formatter.rb
+++ app/services/share_formatter.rb
@@ -1,8 +1,8 @@
 class ShareFormatter
   def self.share_user(user, timezone = nil)
     ["#{user.total_kudos_today(timezone)} kudos tday, ",
-     "#{user.total_kudos_yesterday(timezone)} yday\n\n",
-     Rails.application.routes.url_helpers.u_url(user.to_param)]
-     .join("")
+      "#{user.total_kudos_yesterday(timezone)} yday\n\n",
+      Rails.application.routes.url_helpers.u_url(user.to_param)]
+      .join("")
   end
 end
