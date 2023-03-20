e6489af1f9d128efe1ed19055e0536f85031da3c
sethherr <seth.william.herr@gmail.com>
2023-03-20 13:29:36 -0700

    add approve/disapprove

--- app/controllers/following_controller.rb
+++ app/controllers/following_controller.rb
@@ -14,6 +14,26 @@ class FollowingController < ApplicationController
     redirect_back(fallback_location: user_root_url, status: :see_other)
   end
 
+  def approve
+    if user_follower&.update(approved: true)
+      flash[:success] = "Approved follower: #{@user.username}"
+    elsif @user.blank?
+      flash[:error] = "Unable to find user: '#{params[:id]}'"
+    else
+      flash[:error] = "Unable to approve follower: #{@user.username}, #{user_following.errors.full_messages.to_sentence}"
+    end
+  end
+
+  def unapprove
+    if user_follower&.update(approved: false)
+      flash[:success] = "Un-approved follower: #{@user.username}"
+    elsif @user.blank?
+      flash[:error] = "Unable to find user: '#{params[:id]}'"
+    else
+      flash[:error] = "Unable to un-approve follower: #{@user.username}, #{user_following.errors.full_messages.to_sentence}"
+    end
+  end
+
   def destroy
     if @user.blank?
       flash[:error] = "Unable to find user: #{params[:id]}"
@@ -33,6 +53,10 @@ class FollowingController < ApplicationController
 
   def user_following_match
     UserFollowing.where(user_id: current_user.id, following_id: @user&.id)
+  end
+
+  def user_follower
+    UserFollowing.where(user_id: @user&.id, following_id: current_user.id)
   end
 
   def find_user
