7163625d3182b1eddbc34f63dabf2471ea2c85f6
sethherr <seth.william.herr@gmail.com>
2023-03-31 12:01:58 -0700

    add striped table

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme') // eslint-disable-line
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   plugins: {
@@ -23,18 +24,8 @@ module.exports = {
           success: '#75b798', // alternative: #a3cfbb
           error: '#fecba1', // orange
           bodytext: '#212529', // gray-900
-          gray: {
-            DEFAULT: '#adb5bd', // bootstrap grays
-            100: '#f8f9fa',
-            200: '#e9ecef',
-            300: '#dee2e6',
-            400: '#ced4da',
-            500: '#adb5bd',
-            600: '#6c757d',
-            700: '#495057',
-            800: '#343a40',
-            900: '#212529'
-          }
+          gray: colors.gray,
+          slate: colors.slate
         },
         extend: {
           spacing: {
