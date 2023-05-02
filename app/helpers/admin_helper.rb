41cecb68501caf48e7f356061f440c1f1915d27f
sethherr <seth.william.herr@gmail.com>
2023-05-01 18:17:32 -0700

    lint

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,7 +2,7 @@ module AdminHelper
   COUNTED_META_ATTRS = (MetadataAttributer::ATTR_KEYS - %i[canonical_url paywall publisher_name]).map(&:to_s).freeze
 
   def missing_meta_count(citation)
-    values_count = citation.attributes.slice(*COUNTED_META_ATTRS).values.reject(&:blank?).count
+    values_count = citation.attributes.slice(*COUNTED_META_ATTRS).values.count { |element| !element.blank? }
     missing_count = COUNTED_META_ATTRS.count - values_count
     klass = if missing_count > 3
       "text-error"
