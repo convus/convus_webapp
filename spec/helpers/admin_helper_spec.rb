31a61b7b1631666babaae756ed939ffa601101cf
sethherr <seth.william.herr@gmail.com>
2023-05-02 20:55:48 -0700

    helper spec

--- spec/helpers/admin_helper_spec.rb
+++ spec/helpers/admin_helper_spec.rb
@@ -5,13 +5,13 @@ require "rails_helper"
 RSpec.describe AdminHelper, type: :helper do
   describe "missing_meta_count" do
     let(:citation) { Citation.new }
-    it "is 5" do
-      expect(missing_meta_count(citation)).to eq("<span class=\"text-error\">5</span>")
+    it "is full count" do
+      expect(missing_meta_count(citation)).to eq("<span class=\"text-error\">6</span>")
     end
     context "attrs" do
       let(:citation) { Citation.new(authors: "dddd", description: "fff", word_count: 222, published_at: Time.current) }
       it "is less" do
-        expect(missing_meta_count(citation)).to eq("<span class=\"text-success\">1</span>")
+        expect(missing_meta_count(citation)).to eq("<span class=\"text-success\">2</span>")
       end
     end
   end
