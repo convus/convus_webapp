c0476bdedd3a234a87c6973d27dc1c824557692b
Seth Herr <seth.william.herr@gmail.com>
2023-08-20 11:39:33 +0200

    Not correct direction

--- app/models/citation_quiz.rb
+++ app/models/citation_quiz.rb
@@ -1,2 +1,21 @@
 class CitationQuiz < ApplicationRecord
+  DEFAULT_PROMPT = (ENV["QUIZ_PROMPT"] || "").freeze
+
+  STATUS_ENUM = {
+    pending: 0,
+    active: 1,
+    replace: 2
+  }.freeze
+
+  enum status: STATUS_ENUM
+
+  belongs_to :citation
+  has_many :citation_quiz_questions
+
+  before_validation :set_calculated_attributes
+
+  def set_calculated_attributes
+    self.prompt = DEFAULT_PROMPT if prompt.blank?
+
+  end
 end
--- db/migrate/20230818102227_create_citation_quizzes.rb
+++ db/migrate/20230818102227_create_citation_quizzes.rb
@@ -2,9 +2,9 @@ class CreateCitationQuizzes < ActiveRecord::Migration[7.0]
   def change
     create_table :citation_quizzes do |t|
       t.references :citation, foreign_key: true
-      t.boolean :replaced, null: false, default: false
       t.text :prompt
 
+      t.integer :status, default: 0
 
       t.timestamps
     end
