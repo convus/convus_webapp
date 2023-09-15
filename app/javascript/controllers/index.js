5d446dff347912b54e0d3ff945d90fc8a5ae4131
Seth Herr <seth.william.herr@gmail.com>
2023-09-15 10:10:43 -0700

    add auto-reload

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,7 +7,9 @@ import { application } from './application'
 import RatingsFilterController from './ratings_filter_controller'
 import SortableRatingsController from './sortable_ratings_controller'
 import AdminCurrentHeaderController from './admin_current_header_controller'
+import ReloadPageTimerController from './reload_page_timer_controller'
 
 application.register('ratings-filter', RatingsFilterController)
 application.register('sortable-ratings', SortableRatingsController)
 application.register('admin-current-header', AdminCurrentHeaderController)
+application.register('reload-page-timer', ReloadPageTimerController)
